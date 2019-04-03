8d48f0877ebba34ce55357e7086cfb9f65a483a2
Rohit Goyal <39431001+RohitGoyal24@users.noreply.github.com>
2019-04-03 23:59:44 +0530

    Add files via upload

---
+++ src/UserList.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+class UserList extends Component{
+    render(){
+        return (
+            <div>
+            <h2>UserList</h2>
+            <ul>
+            <ol>
+                Name:Sachin - Age - 10
+            </ol>
+            <ol>
+                Name:Som - Age - 20
+            </ol>
+            <ol>
+                Name:Suraj - Age -
+            </ol>
+            <ol>
+                Name:Saurav - Age -
+            </ol>
+            <ol>
+                Name:Rohan - Age -
+            </ol>
+            </ul>
+            </div>
+        );
+    }
+}
+export default UserList;
